2d4868496ce971bfd7e32be9302f7e5bfc2f09d8
Matt Kotsenas <matt.kotsenas@gmail.com>
2015-04-02 00:16:39 -0700

    Add Models project
    - Add placeholder syndication item

---
+++ AutoRss/Models/SyndicationItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StrongerTypes.NonNullable;
+using System.Runtime.Serialization;
+
+namespace Models
+{
+    [DataContract]
+    public class SyndicationItem
+    {
+        [DataMember]
+        public NonNullable<string> Name { get; private set; }
+        [DataMember]
+        public NonNullable<Uri> DetailsLink { get; private set; }
+        [DataMember]
+        public NonNullable<Uri> DownloadLink { get; private set; }
+        [DataMember]
+        public DateTime Created { get; private set; }
+
+        public SyndicationItem(NonNullable<string> name, NonNullable<Uri> detailsLink, NonNullable<Uri> downloadLink, DateTime created)
+        {
+            Name = name;
+            DetailsLink = detailsLink;
+            DownloadLink = downloadLink;
+            Created = created;
+        }
+    }
+}
