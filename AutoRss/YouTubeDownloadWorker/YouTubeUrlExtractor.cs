6b0108c687e28eb503f0693042a9e6ecfb1d7049
Matt Kotsenas <matt.kotsenas@gmail.com>
2015-04-10 23:41:24 -0700

    Add YouTubeUrlExtractor and tests
    - For a given Url, try to find the download link

---
+++ AutoRss/YouTubeDownloadWorker/YouTubeUrlExtractor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using YoutubeExtractor;
+
+namespace AutoRss.YouTubeDownloadWorker
+{
+    public class YouTubeUrlExtractor
+    {
+        public string Download(string youtubeLink)
+        {
+            IEnumerable<VideoInfo> videoInfos;
+            try
+            {
+                videoInfos = DownloadUrlResolver.GetDownloadUrls(youtubeLink);
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
+
+            var video = videoInfos
+                    .Where(info => info.VideoType == VideoType.Mp4)
+                    .OrderByDescending(info => info.Resolution).FirstOrDefault();
+
+            if (video == null) return null;
+
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+            return video.DownloadUrl;
+        }
+    }
+}
---
+++ AutoRss/YouTubeDownloadWorkerTests/YouTubeUrlExtractorTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoRss.YouTubeDownloadWorker;
+using Machine.Specifications;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Local
+
+namespace AutoRss.YouTubeDownloadWorerTests
+{
+    public abstract class YouTubeUrlExtractorTestsContext
+    {
+        Establish context = () =>
+        {
+            Subject = new YouTubeUrlExtractor();
+        };
+
+        protected static YouTubeUrlExtractor Subject;
+        protected static string Url;
+    }
+
+    [Subject(typeof (YouTubeUrlExtractor))]
+    public class When_the_YouTubeUrlExtractor_is_given_a_link_to_a_YouTube_page : YouTubeUrlExtractorTestsContext
+    {
+        Because of = () => Url = Subject.Download("https://www.youtube.com/watch?v=4w1e_JVhtxs");
+
+        It should_return_the_download_url = () => Url.ShouldNotBeNull();
+    }
+
+    [Subject(typeof (YouTubeUrlExtractor))]
+    public class When_the_YouTubeUrlExtractor_is_given_a_link_to_a_non_existent_page : YouTubeUrlExtractorTestsContext
+    {
+        Because of = () => Url = Subject.Download("http://example.org");
+
+        It should_return_null = () => Url.ShouldBeNull();
+    }
+}
